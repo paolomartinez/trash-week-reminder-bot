bf3aad5049fb8593cb0e3df9cbcaba7e46dab1ee
Paolo Martinez <paolomartinezucsd@gmail.com>
2019-10-20 20:00:14 -0700

    Fixing indentation again

--- app.py
+++ app.py
@@ -20,11 +20,11 @@ def webhook():
   return "ok", 200
 
 def send_message(msg):
-    url = 'https://api.groupme.com/v3/bots/post'
+    url  = 'https://api.groupme.com/v3/bots/post'
 
     data = {
-        'bot_id'  : os.getenv('GROUPME_BOT_ID'),
-        'text'    : msg,    
-    }
-    request = Request(url, urlencode.data).encode())
+            'bot_id' : os.getenv('GROUPME_BOT_ID'),
+            'text'   : msg,
+            }
+    request = Request(url, urlencode(data).encode())
     json = urlopen(request).read().decode()
