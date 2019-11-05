0b5e3c5a36e799a2631e350e15c3b11b91f57da2
Paolo Martinez <paolomartinezucsd@gmail.com>
2019-11-04 22:23:52 -0800

    12+10=22 10:26pm

--- scheduled-jobs.py
+++ scheduled-jobs.py
@@ -9,7 +9,9 @@ def rent_payment_reminder():
     msg = 'Reminder to pay Mike rent!'
     app.send_message(msg)
 
-@sched.scheduled_job('cron', day=4, hour=20, minute=18)
+# Scheduled job to send rent reminder message on the 25th of every month at 7:30pm
+#@sched.scheduled_job('cron', day=25, hour=19, minute=30)
+@sched.scheduled_job('cron', day='mon', hour=22, minute=26)
 def rent_reminder_job():
     rent_payment_reminder()
 
