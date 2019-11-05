0e5434abb6673616aacaa59a370460cbaac0c84e
Paolo Martinez <paolomartinezucsd@gmail.com>
2019-11-04 22:26:45 -0800

    Changed scheduled job to 25th every month at 7:30pm

--- scheduled-jobs.py
+++ scheduled-jobs.py
@@ -9,9 +9,8 @@ def rent_payment_reminder():
     msg = 'Reminder to pay Mike rent!'
     app.send_message(msg)
 
-# Scheduled job to send rent reminder message on the 25th of every month at 7:30pm
-#@sched.scheduled_job('cron', day=25, hour=19, minute=30)
-@sched.scheduled_job('cron', day=4, hour=22, minute=26)
+# Send rent reminder message on the 25th of every month at 7:30pm
+@sched.scheduled_job('cron', day=25, hour=19, minute=30)
 def rent_reminder_job():
     rent_payment_reminder()
 
